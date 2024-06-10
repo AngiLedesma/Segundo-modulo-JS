34a0048536bd4f83cae5ab25dc3748dd0d0cf219
Agñel Gabriela Ledesma <angiledesmag@gmail.com>
2024-06-10 19:21:09 +0200

    clase 17

---
+++ clase 17 js/ejemplo01.js
@@ -0,0 +1,10 @@
+const numeros = [1, 2, 3, 4, 5, 6, , 8, 9, 10]
+
+const pares = numeros.filter(num => num %2 === 0);
+
+
+//==>arrow function
+//el porerador > devuelve de la divisiom de 2 numeros
+
+console.log(numeros)
+console.log(pares)
